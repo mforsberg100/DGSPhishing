2af45951a5085b84d8e8ef004e2a43be6f905e41
Matthew Forsberg <Matthew.Forsberg@dgs.ca.gov>
2020-07-15 15:27:58 -0700

    test

---
+++ DGSPhishing/Ribbon1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace DGSPhishing
+{
+    partial class ReportSpam : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public ReportSpam()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Home = this.Factory.CreateRibbonTab();
+            this.Custom = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.Home.SuspendLayout();
+            this.Custom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Home
+            // 
+            this.Home.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.Home.ControlId.OfficeId = "TabMail";
+            this.Home.Groups.Add(this.Custom);
+            this.Home.Label = "TabMail";
+            this.Home.Name = "Home";
+            // 
+            // Custom
+            // 
+            this.Custom.Items.Add(this.button1);
+            this.Custom.Label = "DGS";
+            this.Custom.Name = "Custom";
+            // 
+            // button1
+            // 
+            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button1.Image = global::DGSPhishing.Properties.Resources._3d_render_robot_with_shield_1048_5702;
+            this.button1.Label = "Report Phishing";
+            this.button1.Name = "button1";
+            this.button1.ScreenTip = "Report selected email as spam";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click_1);
+            // 
+            // ReportSpam
+            // 
+            this.Name = "ReportSpam";
+            this.RibbonType = "Microsoft.Outlook.Explorer";
+            this.Tabs.Add(this.Home);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.Home.ResumeLayout(false);
+            this.Home.PerformLayout();
+            this.Custom.ResumeLayout(false);
+            this.Custom.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab Home;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup Custom;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal ReportSpam Ribbon1
+        {
+            get { return this.GetRibbon<ReportSpam>(); }
+        }
+    }
+}
---
+++ DGSPhishing/Ribbon1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Office = Microsoft.Office.Core;
+using System.Windows.Forms;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace DGSPhishing
+{
+
+    public partial class ReportSpam
+    {
+        private string path = @"c:\DGSISOOutlookAddin\";
+        private string fileName = "settings.ini";
+        private string email = "dgsphishing@dgs.ca.gov";
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            //if the settings file does not exist, create it
+            if (!File.Exists(path + fileName))
+            {
+                CreateFile();
+            }
+        }
+
+        private void CreateFile()
+        {
+            try
+            {
+                Directory.CreateDirectory(path);
+                File.WriteAllText(path + fileName, email);
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show(@"Error creating/writing to email settings file " + path + fileName);
+            }
+        }
+
+        private string ReadFile()
+        {
+            try
+            {
+                return File.ReadAllText(path + fileName);
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("Unable to read settings file " + path + fileName);
+                return "";
+            }
+        }
+
+     
+            private void button1_Click_1(object sender, RibbonControlEventArgs e)
+            {
+                string MessageBoxTitle = "Report Phishing Email";
+                string MessageBoxContent = "Do you want to report this email to the ISO as potential Phishing?";
+                DialogResult dialogResult = MessageBox.Show(MessageBoxContent, MessageBoxTitle, MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+
+
+                    Outlook.Application application = new Outlook.Application();
+                    Outlook.NameSpace ns = application.GetNamespace("MAPI");
+
+                    if (application.ActiveExplorer().Selection.Count > 0)
+                    {
+                        //get selected mail item
+                        Object selectedObject = application.ActiveExplorer().Selection[1];
+                        if (selectedObject is Outlook.MailItem)
+                        {
+                            Outlook.MailItem selectedMail = (Outlook.MailItem)selectedObject;
+
+                            //create message
+                            Outlook.MailItem newMail = application.CreateItem(Outlook.OlItemType.olMailItem) as Outlook.MailItem;
+                            newMail.Recipients.Add(ReadFile());
+                            newMail.Subject = "Phishing";
+                            newMail.Body = "For Ticket Creation";
+                            newMail.Attachments.Add(selectedMail, Microsoft.Office.Interop.Outlook.OlAttachmentType.olEmbeddeditem);
+
+
+
+                            newMail.Send();
+                            selectedMail.Delete();
+
+                            System.Windows.Forms.MessageBox.Show("Thank you for reporting the message as susipcious.\r\n\r\nYou will be contacted by the ISO Shortly.","Thank You");
+                        }
+                    }
+                
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                System.Windows.Forms.MessageBox.Show("You must select a message to report.");
+            }
+    }
+          
+          
+
+
+            }
+                
+            }
+           
+    
